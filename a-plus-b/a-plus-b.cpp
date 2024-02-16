96ff5882cd77763c675bdec90b54d874fe4fc465
MukhinIvan <mukhiv97@yandex.ru>
2024-02-16 22:21:17 +0500

    Добавить задачу 1 "A + B" и файл README.md

---
+++ a-plus-b/a-plus-b.cpp
@@ -0,0 +1,13 @@
+// Задача 1: "A + B"
+// a-plus-b.cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    cout << a + b << endl;
+
+    return 0;
+}
